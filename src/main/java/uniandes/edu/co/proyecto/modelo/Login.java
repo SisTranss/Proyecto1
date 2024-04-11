eeeabc17bb558145a2650785f9520f886ef5619a
Thomas Bonnett <t.bonnett@uniandes.edu.co>
2024-04-11 12:55:50 -0500

    Arreglo PK

--- src/main/java/uniandes/edu/co/proyecto/modelo/Login.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Login.java
@@ -1,27 +1,35 @@
 package uniandes.edu.co.proyecto.modelo;
 
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "logins")
 public class Login {
 
-    @Id
-    private String login;
+    @EmbeddedId
+    private LoginPk loginPk;
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
     public String getLogin() {
-        return login;
+        return loginPk.getUsername();
     }
 
     public void setLogin(String login) {
-        this.login = login;
+        this.loginPk.setUsername(login);
+    }
+
+    public String getPassword() {
+        return loginPk.getPassword();
+    }
+
+    public void setPassword(String password) {
+        this.loginPk.setPassword(password);
     }
 
     public Long getId() {
