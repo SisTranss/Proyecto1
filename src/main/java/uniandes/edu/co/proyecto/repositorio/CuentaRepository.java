039b81690e7cc93548065c83780eabbb2ebee24a
Camilafajardo22 <c.fajardos@uniandes.edu.co>
2024-04-28 21:16:45 -0500

    extra

--- src/main/java/uniandes/edu/co/proyecto/repositorio/CuentaRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/CuentaRepository.java
@@ -16,8 +16,7 @@ import java.sql.Date;
 
 public interface CuentaRepository extends JpaRepository<Cuenta, Integer> {
 
-    @Query(value = "SELECT * FROM cuentas ", nativeQuery = true)
-    Collection<Cuenta> darCuentas();
+    //GENERAL
 
     @Query(value = "SELECT * FROM cuentas WHERE id = :id", nativeQuery = true)
     Cuenta darCuenta(@Param("id") Integer id);
@@ -26,52 +25,8 @@ public interface CuentaRepository extends JpaRepository<Cuenta, Integer> {
     @Transactional
     @Query(value = "INSERT INTO cuentas (tipo, id, estado, saldo, ultima_transaccion, num_doc_cliente, id_oficina) VALUES (:tipo, general_seq.nextval, :estado, :saldo, :ultima_transaccion, :num_doc_cliente, :id_oficina)", nativeQuery = true)
     void insertarCuenta(@Param("tipo") String tipo, @Param("estado") String estado, @Param("saldo") Float saldo,
-            @Param("ultima_transaccion") Date ultima_transaccion, @Param("num_doc_cliente") Integer num_doc_cliente,
-            @Param("id_oficina") Integer id_oficina);
-
-
-    @Query(value = "SELECT * FROM cuentas WHERE id_oficina = :id_oficina", nativeQuery = true)
-    Collection<Cuenta> darCuentasPorIDoficina(@Param("id_oficina") Integer id_oficina);
-
-    @Query(value = "SELECT * FROM cuentas WHERE num_doc_cliente = :num_doc_cliente", nativeQuery = true)
-    Collection<Cuenta> darCuentasPorCliente2(@Param("num_doc_cliente") Integer num_doc_cliente);
-
-
-
-    @Query(value = "SELECT c.* FROM cuentas c INNER JOIN usuarios u ON c.num_doc_cliente = u.id WHERE u.num_doc = :num_doc", nativeQuery = true)
-    Collection<Cuenta> darCuentasPorCliente(@Param("num_doc") String num_doc);
-
-    @Query(value = "SELECT * FROM cuentas WHERE tipo = :tipo", nativeQuery = true)
-    Collection<Cuenta> darCuentasPorTipo(@Param("tipo") String tipo);
-
-    @Query(value = "SELECT * FROM cuentas WHERE saldo BETWEEN :min_saldo AND :max_saldo", nativeQuery = true)
-    Collection<Cuenta> darCuentasPorRangoSaldos(@Param("min_saldo") Integer min_saldo, @Param("max_saldo") Integer max_saldo);
-
-    @Query(value = "SELECT * FROM cuentas WHERE TRUNC(ultima_transaccion) = TO_DATE(:ultima_transaccion, 'YYYY-MM-DD')", nativeQuery = true)
-    Collection<Cuenta> darCuentasPorUltimoMov(@Param("ultima_transaccion") String ultima_transaccion);
-
-
-    @Query(value = "SELECT * FROM cuentas WHERE tipo = :tipo AND id_oficina = :id_oficina", nativeQuery = true)
-    Collection<Cuenta> darCuentasPorTipoCuentayIDoficina(@Param("tipo") String tipo, @Param("id_oficina") Integer id_oficina);
-
-    @Query(value = "SELECT * FROM cuentas WHERE saldo BETWEEN :min_saldo AND :max_saldo AND id_oficina = :id_oficina", nativeQuery = true)
-    Collection<Cuenta> darCuentasPorRangoSaldosyIDoficina(@Param("min_saldo") Integer min_saldo, @Param("max_saldo") Integer max_saldo, @Param("id_oficina") Integer id_oficina);
-
-    @Query(value = "SELECT * FROM cuentas WHERE TRUNC(ultima_transaccion) = TO_DATE(:ultima_transaccion, 'YYYY-MM-DD') AND id_oficina = :id_oficina", nativeQuery = true)
-    Collection<Cuenta> darCuentasPorUltimoMovyIDoficina(@Param("ultima_transaccion") String ultima_transaccion, @Param("id_oficina") Integer id_oficina);
-
-
-    @Query(value = "SELECT * FROM cuentas WHERE tipo = :tipo AND num_doc_cliente = :num_doc_cliente", nativeQuery = true)
-    Collection<Cuenta> darCuentasPorTipoCuentayCliente(@Param("num_doc_cliente") Integer num_doc_cliente, @Param("tipo") String tipo);
-
-    @Query(value = "SELECT * FROM cuentas WHERE saldo BETWEEN :min_saldo AND :max_saldo AND num_doc_cliente = :num_doc_cliente", nativeQuery = true)
-    Collection<Cuenta> darCuentasPorRangoSaldosyCliente(@Param("min_saldo") Integer min_saldo, @Param("max_saldo") Integer max_saldo, @Param("num_doc_cliente") Integer num_doc_cliente);
-
-    @Query(value = "SELECT * FROM cuentas WHERE TRUNC(ultima_transaccion) = TO_DATE(:ultima_transaccion, 'YYYY-MM-DD') AND num_doc_cliente = :num_doc_cliente", nativeQuery = true)
-    Collection<Cuenta> darCuentasPorUltimoMovyCliente(@Param("ultima_transaccion") String ultima_transaccion, @Param("num_doc_cliente") Integer num_doc_cliente);
-
-
-
+                        @Param("ultima_transaccion") Date ultima_transaccion, @Param("num_doc_cliente") Integer num_doc_cliente,
+                        @Param("id_oficina") Integer id_oficina);
 
     @Modifying
     @Transactional
@@ -91,4 +46,55 @@ public interface CuentaRepository extends JpaRepository<Cuenta, Integer> {
     
     @Query(value = "SELECT saldo from cuentas WHERE id =:id", nativeQuery = true)
     Integer darDineroActual(@Param("id") Integer id);
+
+    //TODAS LAS CUENTAS
+
+    @Query(value = "SELECT * FROM cuentas ", nativeQuery = true)
+    Collection<Cuenta> darCuentas();
+
+    @Query(value = "SELECT * FROM cuentas WHERE tipo = :tipo", nativeQuery = true)
+    Collection<Cuenta> darCuentasPorTipo(@Param("tipo") String tipo);
+
+    @Query(value = "SELECT c.* FROM cuentas c INNER JOIN usuarios u ON c.num_doc_cliente = u.id WHERE u.num_doc = :num_doc", nativeQuery = true)
+    Collection<Cuenta> darCuentasPorCliente(@Param("num_doc") String num_doc);
+
+    @Query(value = "SELECT * FROM cuentas WHERE saldo BETWEEN :min_saldo AND :max_saldo", nativeQuery = true)
+    Collection<Cuenta> darCuentasPorRangoSaldos(@Param("min_saldo") Integer min_saldo, @Param("max_saldo") Integer max_saldo);
+
+    @Query(value = "SELECT * FROM cuentas WHERE TRUNC(ultima_transaccion) = TO_DATE(:ultima_transaccion, 'YYYY-MM-DD')", nativeQuery = true)
+    Collection<Cuenta> darCuentasPorUltimoMov(@Param("ultima_transaccion") String ultima_transaccion);
+
+
+    //POR OFICINA
+
+    @Query(value = "SELECT * FROM cuentas WHERE id_oficina = :id_oficina", nativeQuery = true)
+    Collection<Cuenta> darCuentasPorIDoficina(@Param("id_oficina") Integer id_oficina);
+    
+    @Query(value = "SELECT * FROM cuentas WHERE tipo = :tipo AND id_oficina = :id_oficina", nativeQuery = true)
+    Collection<Cuenta> darCuentasPorTipoCuentayIDoficina(@Param("tipo") String tipo, @Param("id_oficina") Integer id_oficina);
+
+    @Query(value = "SELECT * FROM cuentas WHERE saldo BETWEEN :min_saldo AND :max_saldo AND id_oficina = :id_oficina", nativeQuery = true)
+    Collection<Cuenta> darCuentasPorRangoSaldosyIDoficina(@Param("min_saldo") Integer min_saldo, @Param("max_saldo") Integer max_saldo, @Param("id_oficina") Integer id_oficina);
+
+    @Query(value = "SELECT * FROM cuentas WHERE TRUNC(ultima_transaccion) = TO_DATE(:ultima_transaccion, 'YYYY-MM-DD') AND id_oficina = :id_oficina", nativeQuery = true)
+    Collection<Cuenta> darCuentasPorUltimoMovyIDoficina(@Param("ultima_transaccion") String ultima_transaccion, @Param("id_oficina") Integer id_oficina);
+
+    @Query(value = "SELECT * FROM cuentas WHERE num_doc_cliente = :num_doc_cliente AND id_oficina = :id_oficina", nativeQuery = true)
+    Collection<Cuenta> darCuentasPorClienteyIDoficina(@Param("num_doc_cliente") Integer num_doc_cliente, @Param("id_oficina") Integer id_oficina);
+
+
+    //POR CLIENTE
+
+    @Query(value = "SELECT * FROM cuentas WHERE num_doc_cliente = :num_doc_cliente", nativeQuery = true)
+    Collection<Cuenta> darCuentasPorCliente2(@Param("num_doc_cliente") Integer num_doc_cliente);
+
+    @Query(value = "SELECT * FROM cuentas WHERE tipo = :tipo AND num_doc_cliente = :num_doc_cliente", nativeQuery = true)
+    Collection<Cuenta> darCuentasPorTipoCuentayCliente(@Param("num_doc_cliente") Integer num_doc_cliente, @Param("tipo") String tipo);
+
+    @Query(value = "SELECT * FROM cuentas WHERE saldo BETWEEN :min_saldo AND :max_saldo AND num_doc_cliente = :num_doc_cliente", nativeQuery = true)
+    Collection<Cuenta> darCuentasPorRangoSaldosyCliente(@Param("min_saldo") Integer min_saldo, @Param("max_saldo") Integer max_saldo, @Param("num_doc_cliente") Integer num_doc_cliente);
+
+    @Query(value = "SELECT * FROM cuentas WHERE TRUNC(ultima_transaccion) = TO_DATE(:ultima_transaccion, 'YYYY-MM-DD') AND num_doc_cliente = :num_doc_cliente", nativeQuery = true)
+    Collection<Cuenta> darCuentasPorUltimoMovyCliente(@Param("ultima_transaccion") String ultima_transaccion, @Param("num_doc_cliente") Integer num_doc_cliente);
+
 }
